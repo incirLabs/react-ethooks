50fa32bc1ff1c8b21cdfcc906181e67d9fffaeea
Uğur Eren <contact@ugureren.net>
2022-05-09 18:32:01 +0300

    useContractWrite hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export {default as useAccount} from './useAccount';
 export {default as useConnect} from './useConnect';
 export {default as useContract} from './useContract';
 export {default as useContractRead} from './useContractRead';
+export {default as useContractWrite} from './useContractWrite';
---
+++ src/hooks/useContractWrite.ts
@@ -0,0 +1,41 @@
+import {ethers} from 'ethers';
+import {useCallback} from 'react';
+import useConnect from './useConnect';
+import {useRootContext} from '../contexts';
+
+const useContractWrite = (contract: ethers.Contract, method: string, defaultArgs: Args = []) => {
+  const root = useRootContext();
+
+  const {connect} = useConnect();
+
+  const write = useCallback(
+    async (
+      args = defaultArgs,
+      listeners: Record<string, ethers.providers.Listener> = {},
+    ): Promise<ContractResult> => {
+      if (!root.signer) await connect();
+
+      let contractWithSigner = contract;
+      if (root.signer) contractWithSigner = contract.connect(root.signer);
+
+      try {
+        const result = await contractWithSigner[method](...args);
+
+        if (listeners && typeof listeners === 'object') {
+          Object.entries(listeners).forEach(([name, event]) => {
+            contractWithSigner.on(name, event);
+          });
+        }
+
+        return {status: true, data: result};
+      } catch (err) {
+        return {status: false, error: err as Error};
+      }
+    },
+    [contract, method, defaultArgs, connect, root.signer],
+  );
+
+  return write;
+};
+
+export default useContractWrite;
