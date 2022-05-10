845c49136926e430ab306969d0b9f2e72218fdc1
Uğur Eren <contact@ugureren.net>
2022-05-10 13:06:34 +0300

    fix: export useRC20 hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,4 +3,5 @@ export {default as useConnect} from './useConnect';
 export {default as useContract} from './useContract';
 export {default as useContractRead} from './useContractRead';
 export {default as useContractWrite} from './useContractWrite';
+export {default as useERC20} from './useERC20';
 export {default as useSigner} from './useSigner';
