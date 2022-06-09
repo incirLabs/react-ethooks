3a1204e1324f6b2a23d9e6a316b71a6be326cc33
Uğur Eren <contact@ugureren.net>
2022-06-09 08:30:03 +0300

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './hooks';
 export * from './contexts';
 export * from './providers';
+export * from './types';
 
 export {default as AutoChangeChain} from './utils/AutoChangeChain';
 
