30551d19426e911d65329fa0f95311d05f1d6f22
Uğur Eren <contact@ugureren.net>
2022-06-13 17:10:34 +0300

    docs: create docusaurus project

---
+++ docs/docs/installation.md
@@ -0,0 +1,39 @@
+---
+id: installation
+title: Installation
+sidebar_position: 1
+slug: /
+---
+
+```mdx-code-block
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+```
+
+React Ethooks can be installed to any React project using any npm package manager.
+
+:::caution
+  React Ethooks depends on ethers library. So you must also install ethers.js library.
+:::
+
+<Tabs>
+  <TabItem value="yarn">
+
+```bash
+yarn add @incirlabs/react-ethooks ethers
+```
+
+  </TabItem>
+
+  <TabItem value="npm">
+
+```bash
+npm install --save @incirlabs/react-ethooks ethers
+```
+
+  </TabItem>
+</Tabs>
+
+You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
+
+That's all! You can now use React Ethooks in your React project.
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,85 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+// @ts-check
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const Name = 'React Ethooks';
+const GithubUrl = 'https://github.com/incirLabs/react-ethooks';
+const NpmUrl = 'https://www.npmjs.com/package/react-ethooks';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: Name,
+  tagline: '',
+  url: 'https://incirlabs.github.io',
+  baseUrl: '/react-ethooks/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'incirLabs', // Usually your GitHub org/user name.
+  projectName: 'react-ethooks', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: `${GithubUrl}/tree/main/docs/`,
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+      },
+      navbar: {
+        title: Name,
+        items: [
+          {
+            href: GithubUrl,
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            label: 'Github',
+            href: GithubUrl,
+          },
+          {
+            label: 'NPM',
+            href: NpmUrl,
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} incirLabs, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
+    }),
+};
+
+module.exports = config;
