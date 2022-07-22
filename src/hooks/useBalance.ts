f12cf09d965b4f0966a43b156b85693031aee564
Uğur Eren <contact@ugureren.net>
2022-07-22 04:33:24 +0300

    refactor: useBalance hook description & refactor

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -1,10 +1,12 @@
-import {useEffect, useMemo, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {BigNumber} from 'ethers';
 import {useProvider} from '../contexts';
 import useAccount from './useAccount';
 
 /**
  * Returns the balance of the currently connected account or the given address.
+ *
+ * @param address - Address to get the balance of. If not given, currently connected account is used.
  */
 const useBalance = (address?: string) => {
   const [balance, setBalance] = useState(BigNumber.from(0));
@@ -20,7 +22,7 @@ const useBalance = (address?: string) => {
     }
   }, [provider, addressToUse]);
 
-  return useMemo(() => balance, [balance]);
+  return balance;
 };
 
 export default useBalance;
